4815a568dcc740b24dfd017bcf969a20f1bf3ee7
derojasjonathan@gmail.com <derojasjonathan@gmail.com@9b861c46-e4be-26d0-0e1f-3d34e108c488>
2014-09-06 19:10:52 +0000

    bug que se qedba pegada a los marcos arreglado!creo xD

--- trunk/VideoJuego/src/main/java/fernando/Pelota.java
+++ trunk/VideoJuego/src/main/java/fernando/Pelota.java
@@ -24,18 +24,38 @@ public class Pelota extends GameComponent<PelotitaScene> {
 		this.setX(nuevaPosicion.getX());
 		this.setY(nuevaPosicion.getY());
 		//choques
+		
+		/*
 		if(getGame().getDisplayWidth() <= nuevaPosicion.getX() + getAppearance().getWidth() || 
-				nuevaPosicion.getX() <= 0){
-			//setX(getGame().getDisplayWidth()-getAppearance().getWidth());
-//			Vector2D vector = new Vector2D(getX()*-1,getY()*-1);
+			nuevaPosicion.getX() <= 0){
 			setDireccion(new Vector2D(-1*getDireccion().getX(), getDireccion().getY()));
 		}else{
 			if(nuevaPosicion.getY() <= 0 || 
-					getGame().getDisplayHeight() <= nuevaPosicion.getY() + getAppearance().getHeight()){
+				getGame().getDisplayHeight() <= nuevaPosicion.getY() + getAppearance().getHeight()){
 				setDireccion(new Vector2D(getDireccion().getX(), -1*getDireccion().getY()));
-			}			
+			}
 		}
-
+		*/
+		
+		if(nuevaPosicion.getY() + getAppearance().getHeight() >= getGame().getDisplayHeight() ){
+			setDireccion(new Vector2D(getDireccion().getX(), -1*getDireccion().getY()));
+			this.setY( getGame().getDisplayHeight()-getAppearance().getHeight());
+			}
+		if(nuevaPosicion.getY() < 0){
+			setDireccion(new Vector2D(getDireccion().getX(), -1*getDireccion().getY()));
+			this.setY(0);
+			}
+		
+		if(nuevaPosicion.getX() + getAppearance().getWidth() >= getGame().getDisplayWidth() ){
+			setDireccion(new Vector2D(-1*getDireccion().getX(), getDireccion().getY()));
+			this.setX( getGame().getDisplayWidth()-getAppearance().getWidth());
+			}
+		if(nuevaPosicion.getX() < 0){
+			setDireccion(new Vector2D(-1*getDireccion().getX(), getDireccion().getY()));
+			this.setX(0);
+			}
+		
+		
 		super.update(deltaState);
 	}
 	
