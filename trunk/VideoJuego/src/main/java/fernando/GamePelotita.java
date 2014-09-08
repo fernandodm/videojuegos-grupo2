a229a7e378f53a3633a6fbc225b418408f706bb0
jgdimeglio@gmail.com <jgdimeglio@gmail.com@9b861c46-e4be-26d0-0e1f-3d34e108c488>
2014-09-08 02:52:23 +0000

    Dibujando cuadraditos de colores(demo)

--- trunk/VideoJuego/src/main/java/fernando/GamePelotita.java
+++ trunk/VideoJuego/src/main/java/fernando/GamePelotita.java
@@ -1,8 +1,16 @@
 package fernando;
 
+import java.awt.Color;
 import java.awt.Dimension;
+
+
+
 import com.uqbar.vainilla.DesktopGameLauncher;
 import com.uqbar.vainilla.Game;
+import com.uqbar.vainilla.GameComponent;
+import com.uqbar.vainilla.GameScene;
+import com.uqbar.vainilla.appearances.Circle;
+import com.uqbar.vainilla.appearances.Rectangle;
 
 public class GamePelotita extends Game {
 
@@ -17,12 +25,30 @@ public class GamePelotita extends Game {
 		PelotitaScene pelotitaScene = new PelotitaScene();
 		Pelota pelota = new Pelota(30, 2, 2, new Vector(5, -2), 900);
 		((PelotitaScene) pelotitaScene).setPelota(pelota);
+		renderElements(pelotitaScene);
 		this.setCurrentScene(pelotitaScene);
+	}
+	
+	
+	public void renderElements(GameScene scene){
+		
+		int length=10;
+		int width =10;
+		
+		Color[] colors={Color.red,Color.black,Color.blue,Color.DARK_GRAY,Color.orange,Color.white};
+		
+		for (int y = 0; y < width; y++) {
+			for (int x = 0; x < length; x++) {
+				Color color=colors[(int)(Math.random()*colors.length)];
+				scene.addComponent(new GameComponent<GameScene>(new Rectangle(color,48,18),x*50, y*20));
+			}
+		}
+		
 	}
 
 	@Override
 	public Dimension getDisplaySize() {
-		return new Dimension(200, 200);
+		return new Dimension(500, 500);
 	}
 
 	@Override
