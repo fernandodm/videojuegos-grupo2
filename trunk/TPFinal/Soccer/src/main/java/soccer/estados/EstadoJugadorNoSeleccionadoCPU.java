4bc27e8c994626a952b6653f2381f4d421469a1d
jgdimeglio@gmail.com <jgdimeglio@gmail.com@9b861c46-e4be-26d0-0e1f-3d34e108c488>
2014-11-17 23:22:00 +0000

    primera version de inteligencia

--- trunk/TPFinal/Soccer/src/main/java/soccer/estados/EstadoJugadorNoSeleccionadoCPU.java
+++ trunk/TPFinal/Soccer/src/main/java/soccer/estados/EstadoJugadorNoSeleccionadoCPU.java
@@ -1,5 +1,6 @@
 package soccer.estados;
 
+import soccer.Direccion;
 import soccer.Jugador;
 
 import com.uqbar.vainilla.DeltaState;
@@ -11,7 +12,46 @@ public class EstadoJugadorNoSeleccionadoCPU extends EstadoJugador {
 	}
 
 	public void update(DeltaState deltaState) {
-
+		if(Utils.distanciaConPelota(this.getJugador().getX(), this.getJugador().getY())> 150
+				){
+			return;
+		}
+		int direccion = Utils.direccionPelota();
+		switch (direccion) {
+		case Direccion.DOWN:
+			this.getJugador().up(deltaState);
+			this.getJugador().ejecutarSprite(deltaState, Direccion.UP,0);
+			break;
+		case Direccion.UP:
+			this.getJugador().down(deltaState);
+			this.getJugador().ejecutarSprite(deltaState, Direccion.DOWN,0);
+			break;
+		case Direccion.RIGHT:
+			this.getJugador().left(deltaState);
+			this.getJugador().ejecutarSprite(deltaState, Direccion.LEFT,0);
+			break;
+		case Direccion.LEFT:
+			this.getJugador().right(deltaState);
+			this.getJugador().ejecutarSprite(deltaState, Direccion.RIGHT,0);
+			break;
+		case Direccion.UPLEFT:
+			this.getJugador().upRight(deltaState);
+			this.getJugador().ejecutarSprite(deltaState, Direccion.UP,0.6);
+			break;
+		case Direccion.UPRIGHT:
+			this.getJugador().upLeft(deltaState);
+			this.getJugador().ejecutarSprite(deltaState, Direccion.UP,-0.6);
+			break;
+		case Direccion.DOWNLEFT:
+			this.getJugador().downRight(deltaState);
+			this.getJugador().ejecutarSprite(deltaState, Direccion.UP,2);
+			break;
+		case Direccion.DOWNRIGHT:
+			this.getJugador().downLeft(deltaState);
+			this.getJugador().ejecutarSprite(deltaState, Direccion.UP,-2);
+			break;
+//		}		
+	}
 	}
 
 }
--- trunk/TPFinal/Soccer/src/main/java/soccer/estados/Utils.java
+++ trunk/TPFinal/Soccer/src/main/java/soccer/estados/Utils.java
@@ -4,52 +4,56 @@ import java.util.List;
 
 import soccer.Arco;
 import soccer.Jugador;
-import soccer.JugadorLocal;
 import soccer.Pelota;
 import soccer.SoccerScene;
 
 public class Utils {
 
-	public static SoccerScene  scene;
-	
-	public static double distanciaConPelota(int x,int y){
-		Pelota pelota=Utils.scene.getPelota();
-		double distanciaActual = Math.sqrt(Math.pow((x - pelota.getX()),2) 
-	 			   + Math.pow((y- pelota.getY()), 2));
+	public static SoccerScene scene;
+
+	public static double distanciaConPelota(double x, double y) {
+		Pelota pelota = Utils.scene.getPelota();
+		double distanciaActual = Math.sqrt(Math.pow((x - pelota.getX()), 2)
+				+ Math.pow((y - pelota.getY()), 2));
 		return distanciaActual;
 	}
-	
-	public static double distanciaConArcoLocal(int x,int y){
-		Arco Arco=Utils.scene.getArcos().get(1);
-		double distanciaActual = Math.sqrt(Math.pow((x - Arco.getX()),2) 
-	 			   + Math.pow((y- Arco.getY()), 2));
+
+	public static double distanciaConArcoLocal(double x, double y) {
+		Arco Arco = Utils.scene.getArcos().get(1);
+		double distanciaActual = Math.sqrt(Math.pow((x - Arco.getX()), 2)
+				+ Math.pow((y - Arco.getY()), 2));
 		return distanciaActual;
 	}
-	
-	public static double distanciaConArcoVisitante(int x,int y){
-		Arco Arco=Utils.scene.getArcos().get(0);
-		double distanciaActual = Math.sqrt(Math.pow((x - Arco.getX()),2) 
-	 			   + Math.pow((y- Arco.getY()), 2));
+
+	public static double distanciaConArcoVisitante(double x, double y) {
+		Arco Arco = Utils.scene.getArcos().get(0);
+		double distanciaActual = Math.sqrt(Math.pow((x - Arco.getX()), 2)
+				+ Math.pow((y - Arco.getY()), 2));
 		return distanciaActual;
 	}
-	
-	public static boolean tienePelotaLocal(){
-		List<Jugador> Jugadores=Utils.scene.getJugadoresLocales();
+
+	public static boolean tienePelotaLocal() {
+		List<Jugador> Jugadores = Utils.scene.getJugadoresLocales();
 		for (Jugador jugador : Jugadores) {
-			if(jugador.flag){
+			if (jugador.flag) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public static boolean tienePelotaVisitante(){
-		List<Jugador> Jugadores=Utils.scene.getJugadoresVisitantes();
+
+	public static boolean tienePelotaVisitante() {
+		List<Jugador> Jugadores = Utils.scene.getJugadoresVisitantes();
 		for (Jugador jugador : Jugadores) {
-			if(jugador.flag){
+			if (jugador.flag) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	public static int direccionPelota() {
+		Pelota pelota = Utils.scene.getPelota();
+		return pelota.getUltimaDireccion();
 	}
 }
