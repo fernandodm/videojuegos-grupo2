e1f302c065d7b1d0866a73ed05ddf3a179cf9bd9
dimegliofernando@gmail.com <dimegliofernando@gmail.com@9b861c46-e4be-26d0-0e1f-3d34e108c488>
2014-11-18 00:11:46 +0000

    cambios en los corner (sin terminar)

--- trunk/TPFinal/Soccer/src/main/java/soccer/estados/EstadoJugadorEnCorner.java
+++ trunk/TPFinal/Soccer/src/main/java/soccer/estados/EstadoJugadorEnCorner.java
@@ -18,21 +18,206 @@ public class EstadoJugadorEnCorner extends EstadoJugador {
 	
 	@Override
 	public void update(DeltaState deltaState) {
-
-		if(this.getJugador().getScene().getPelota().getY() < 50){
+		if(this.getJugador().getScene().getPelota().getY() < 60){
 			if(this.getJugador().getScene().getPelota().getX() < 640){
-				this.ejecutar(Direccion.RIGHT, Direccion.DOWNRIGHT, Direccion.UPRIGHT, 3, 0.3, deltaState, 20, 22);
+				this.ejecutarEsquinaSuperiorIzquierda(deltaState);
 			}else{
-				this.ejecutar(Direccion.LEFT, Direccion.DOWNLEFT, Direccion.UPLEFT, 3, -0.3, deltaState, -22, 20);
+				this.ejecutarEsquinaSuperiorDerecha(deltaState);
 			}
 		}else{
 			if(this.getJugador().getScene().getPelota().getX() < 640){
-				this.ejecutar(Direccion.RIGHT, Direccion.DOWNRIGHT, Direccion.UPRIGHT, 0, 0, deltaState, 20, 22);
+				this.ejecutarEsquinaInferiorIzquierda(deltaState);
 			}else{
-				this.ejecutar(Direccion.LEFT, Direccion.DOWNLEFT, Direccion.UPLEFT, 3, 0.3, deltaState, -22, -21);
+				this.ejecutarEsquinaInferiorDerecha(deltaState);
 			}
 		}
 	}
+		
+	private void ejecutarEsquinaInferiorDerecha(DeltaState deltaState) {
+		int direccion = obtenerDireccion(deltaState);
+		//por si no se apreto ninguna tecla
+		if((direccion == 0 && !seMovio))
+			this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.LEFT);
+		
+		switch (direccion) {
+		case Direccion.UP:
+			
+			if(seMovioUp == 1){
+				this.getJugador().setAppearance(this.getJugador().getImages().get(Direccion.DOWN).get(3));
+				this.getJugador().setY(this.getJugador().getY() - 21);
+				this.getJugador().setX(this.getJugador().getX() - 22);
+				this.getJugador().getLabelSeleccionado().setY(this.getJugador().getLabelSeleccionado().getY() - 21);
+				this.getJugador().getLabelSeleccionado().setX(this.getJugador().getLabelSeleccionado().getX() - 22);
+				this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.DOWNLEFT);
+				seMovioUp--;
+				seMovioDown++;
+			}
+			seMovio = true;
+			break;
+		case Direccion.DOWN:
+
+			if(seMovioDown == 1){
+				this.getJugador().setAppearance(this.getJugador().getImages().get(Direccion.LEFT).get(3).rotate(0.3));
+				this.getJugador().setY(this.getJugador().getY() - 21);
+				this.getJugador().setX(this.getJugador().getX() - 22);
+				this.getJugador().getLabelSeleccionado().setY(this.getJugador().getLabelSeleccionado().getY() - 21);
+				this.getJugador().getLabelSeleccionado().setX(this.getJugador().getLabelSeleccionado().getX() - 22);
+				this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.UPLEFT);
+				seMovioUp++;
+				seMovioDown--;
+			}
+			seMovio = true;
+			break;
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void ejecutarEsquinaInferiorIzquierda(DeltaState deltaState) {
+		int direccion = obtenerDireccion(deltaState);
+		//por si no se apreto ninguna tecla
+		if((direccion == 0 && !seMovio))
+			this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.RIGHT);
+		
+		switch (direccion) {
+		case Direccion.UP:
+			
+			if(seMovioUp == 1){
+				this.getJugador().setAppearance(this.getJugador().getImages().get(Direccion.UP).get(0));
+				this.getJugador().setY(this.getJugador().getY() + 22);
+				this.getJugador().setX(this.getJugador().getX() + 20);
+				this.getJugador().getLabelSeleccionado().setY(this.getJugador().getLabelSeleccionado().getY() + 22);
+				this.getJugador().getLabelSeleccionado().setX(this.getJugador().getLabelSeleccionado().getX() + 20);
+				this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.DOWNRIGHT);
+				seMovioUp--;
+				seMovioDown++;
+			}
+			seMovio = true;
+			break;
+		case Direccion.DOWN:
+
+			if(seMovioDown == 1){
+				this.getJugador().setAppearance(this.getJugador().getImages().get(Direccion.RIGHT).get(0).rotate(0));
+				this.getJugador().setY(this.getJugador().getY() - 22);
+				this.getJugador().setX(this.getJugador().getX() - 20);
+				this.getJugador().getLabelSeleccionado().setY(this.getJugador().getLabelSeleccionado().getY() - 22);
+				this.getJugador().getLabelSeleccionado().setX(this.getJugador().getLabelSeleccionado().getX() - 20);
+				this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.UPRIGHT);
+				seMovioUp++;
+				seMovioDown--;
+			}
+			seMovio = true;
+			break;
+		}
+	}
+	
+	
+	
+	
+	
+	public void ejecutarEsquinaSuperiorDerecha(DeltaState deltaState){
+		
+		int direccion = obtenerDireccion(deltaState);
+		//por si no se apreto ninguna tecla
+		if((direccion == 0 && !seMovio))
+			this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.LEFT);
+		
+		switch (direccion) {
+		case Direccion.UP:
+			
+			if(seMovioUp == 1){
+				this.getJugador().setAppearance(this.getJugador().getImages().get(Direccion.DOWN).get(3));
+				this.getJugador().setY(this.getJugador().getY() - 25);
+				this.getJugador().setX(this.getJugador().getX() - 20);
+				this.getJugador().getLabelSeleccionado().setY(this.getJugador().getLabelSeleccionado().getY() - 25);
+				this.getJugador().getLabelSeleccionado().setX(this.getJugador().getLabelSeleccionado().getX() - 20);
+				this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.DOWNLEFT);
+				seMovioUp--;
+				seMovioDown++;
+			}
+			seMovio = true;
+			break;
+		case Direccion.DOWN:
+
+			if(seMovioDown == 1){
+				this.getJugador().setAppearance(this.getJugador().getImages().get(Direccion.LEFT).get(3).rotate(-0.3));
+				this.getJugador().setY(this.getJugador().getY() + 25);
+				this.getJugador().setX(this.getJugador().getX() + 20);
+				this.getJugador().getLabelSeleccionado().setY(this.getJugador().getLabelSeleccionado().getY() + 25);
+				this.getJugador().getLabelSeleccionado().setX(this.getJugador().getLabelSeleccionado().getX() + 20);
+				this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.UPLEFT);
+				seMovioUp++;
+				seMovioDown--;
+			}
+			seMovio = true;
+			break;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void ejecutarEsquinaSuperiorIzquierda(DeltaState deltaState){
+		int direccion = obtenerDireccion(deltaState);
+		//por si no se apreto ninguna tecla
+		if((direccion == 0 && !seMovio))
+			this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.RIGHT);
+		
+		switch (direccion) {
+		case Direccion.UP:
+			
+			if(seMovioUp == 1){
+				this.getJugador().setAppearance(this.getJugador().getImages().get(Direccion.DOWN).get(3));
+				this.getJugador().setY(this.getJugador().getY() - 22);
+				this.getJugador().setX(this.getJugador().getX() + 20);
+				this.getJugador().getLabelSeleccionado().setY(this.getJugador().getLabelSeleccionado().getY() - 22);
+				this.getJugador().getLabelSeleccionado().setX(this.getJugador().getLabelSeleccionado().getX() + 22);
+				this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.DOWN);
+				seMovioUp--;
+				seMovioDown++;
+			}
+			seMovio = true;
+			break;
+		case Direccion.DOWN:
+
+			if(seMovioDown == 1){
+				this.getJugador().setAppearance(this.getJugador().getImages().get(Direccion.RIGHT).get(0).rotate(0.3));
+				this.getJugador().setY(this.getJugador().getY() + 22);
+				this.getJugador().setX(this.getJugador().getX() - 20);
+				this.getJugador().getLabelSeleccionado().setY(this.getJugador().getLabelSeleccionado().getY() + 22);
+				this.getJugador().getLabelSeleccionado().setX(this.getJugador().getLabelSeleccionado().getX() - 22);
+				this.getJugador().getScene().getPelota().setUltimaDireccion(Direccion.DOWNRIGHT);
+				seMovioUp++;
+				seMovioDown--;
+			}
+			seMovio = true;
+			break;
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	private void ejecutar(int dir, int downright, int upright, int n, double radio, DeltaState deltaState, double sumX, double sumY) {
 		int direccion = obtenerDireccion(deltaState);
